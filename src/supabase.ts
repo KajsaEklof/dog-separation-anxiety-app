de0f9bec715cdcf8593bcb35d511da891c5d15b4
Kajsa1 <kajsaKeklof@gmail.com>
2023-04-23 11:31:10 +0100

    feat: manifest updates

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.VUE_APP_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.VUE_APP_SUPABASE_ANON_KEY as string;
 
-console.log(supabaseUrl)
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- vue.config.ts
+++ vue.config.ts
@@ -3,10 +3,39 @@ import { defineConfig } from '@vue/cli-service';
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    name: 'My App',
+    name: 'Dog Log',
     themeColor: '#4DBA87',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      "name": "Dog Log",
+      "short_name": "Dog Log",
+      "theme_color": "#4DBA87",
+      "icons": [
+        {
+          "src": "./img/icons/android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "./img/icons/android-chrome-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "./img/icons/android-chrome-maskable-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
+        },
+        {
+          "src": "./img/icons/android-chrome-maskable-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ],
+    },
   },
 });
