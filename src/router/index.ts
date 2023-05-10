5c681388a8827837c59a2fe86a817a04e31238e0
Kajsa1 <kajsaKeklof@gmail.com>
2023-05-10 21:03:53 +0100

    fix: optional parameter

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,7 @@ const routes: Array<RouteRecordRaw> = [
         component: TrainingPage,
       },
       {
-        path: '/active-training-session/:id',
+        path: '/active-training-session/:id?',
         component: ActiveTrainingPage,
       },
       {
