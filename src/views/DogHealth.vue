6a3ac308f6f5861507d8a084d77291788bcd5551
Kajsa Eklof <kajsakeklof@gmail.com>
2024-08-28 19:20:06 +0100

    feat: adding weight entry button

--- src/views/DogHealth.vue
+++ src/views/DogHealth.vue
@@ -1,6 +1,16 @@
 <template>
   <v-container>
     <h1>Health</h1>
+    <v-row>
+      <v-col class="d-flex justify-center mt-4 mb-8" >
+        <v-btn color="primary" @click="addWeightEntry">
+          Add weight entry
+          <v-icon class="mr-2">
+            mdi-plus
+          </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
     <weight-chart />
   </v-container>
 </template>
@@ -8,4 +18,8 @@
 
 <script setup lang="ts">
 import WeightChart from "@/components/WeightChart.vue"
+
+async function addWeightEntry() {
+  // Add weight entry
+} 
 </script>
