cd35103e9c28fbb851cc2ed99db4e363708b7071
Kajsa Eklof <kajsakeklof@gmail.com>
2024-09-16 20:16:08 +0100

    feat: add medicine button

--- src/views/DogHealth.vue
+++ src/views/DogHealth.vue
@@ -3,10 +3,20 @@
     <h1>Health</h1>
     <weight-chart :labels="labels" :data="weightData" class="mb-8"/>
     <v-row>
-      <v-col class="d-flex flex-wrap flex-column justify-start mt-4 mb-8" >
+      <v-col class="d-flex flex-wrap flex-column justify-start mt-4" >
         <v-date-input v-model="entryDate" label="Date" variant="outlined" hide-details />
         <v-text-field v-model="weight" label="Weight" variant="outlined"  type="number" suffix="kg" prepend-icon="mdi-weight-kilogram" class="mt-8" hide-details />
-        <v-btn color="primary" class="mt-8" @click="addWeightEntry">Add weight entry</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn color="primary" @click="addWeightEntry">Add weight entry</v-btn>
+    </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h2>Medicine</h2>
+        <v-btn color="primary">Add medicine</v-btn>
       </v-col>
     </v-row>
   </v-container>
